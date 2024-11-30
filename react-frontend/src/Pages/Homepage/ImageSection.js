d0e40ec2aa49cd17fb9a13d09377f602ee17ddf5
Titan antony <antonytitan96@gmail.com>
2024-11-30 18:28:29 +0300

    Chore added an mainsection customised file

---
+++ react-frontend/src/Pages/Homepage/ImageSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './styles/MainSection.module.css';
+import image from "../../assets/1.jpg"
+
+const ImageSection = () => (
+  <div className={styles.imageContainer}>
+    <img
+      src={image} // Replace with your asset
+      alt="Team working on laptops"
+      className={styles.image}
+    />
+  </div>
+);
+
+export default ImageSection;
